b719b52c82e6b39f5efdc40f0abc02de9795c1ae
nathanmullins11 <ndm00002@mix.wvu.edu>
2026-07-12 14:09:40 -0400

    Initial Commit

---
+++ config.h
@@ -0,0 +1,92 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define if you don't have vprintf but do have _doprnt.  */
+/* #undef HAVE_DOPRNT */
+
+/* Define if you have the vprintf function.  */
+#define HAVE_VPRINTF 1
+
+/* Define if the setvbuf function takes the buffering type as its second
+   argument and the buffer pointer as the third, as on System V
+   before release 3.  */
+/* #undef SETVBUF_REVERSED */
+
+/* Define to `unsigned' if <sys/types.h> doesn't define.  */
+/* #undef size_t */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you can safely include both <sys/time.h> and <time.h>.  */
+#define TIME_WITH_SYS_TIME 1
+
+/* Define this if you have the ncurses library and want to use it. */
+#define HAVE_LIBNCURSES 1
+
+/* Define this if you have the curses library and want to use it. */
+/* #undef HAVE_LIBCURSES */
+
+/* Define this if you have (n)curses and want to use it. */
+#define HAVE_CURSES 1
+
+#define HAVE_FUNC_GETHOSTBYNAME_R_6 1
+/* #undef HAVE_FUNC_GETHOSTBYNAME_R_5 */
+/* #undef HAVE_FUNC_GETHOSTBYNAME_R_3 */
+
+/* #undef socklen_t */
+
+/* Define if you have the gettimeofday function.  */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define if you have the select function.  */
+#define HAVE_SELECT 1
+
+/* Define if you have the socket function.  */
+#define HAVE_SOCKET 1
+
+/* Define if you have the strdup function.  */
+#define HAVE_STRDUP 1
+
+/* Define if you have the strerror function.  */
+#define HAVE_STRERROR 1
+
+/* Define if you have the strstr function.  */
+#define HAVE_STRSTR 1
+
+/* Define if you have the <curses.h> header file.  */
+#define HAVE_CURSES_H 1
+
+/* Define if you have the <malloc.h> header file.  */
+#define HAVE_MALLOC_H 1
+
+/* Define if you have the <ncurses.h> header file.  */
+#define HAVE_NCURSES_H 1
+
+/* Define if you have the <strings.h> header file.  */
+#define HAVE_STRINGS_H 1
+
+/* Define if you have the <sys/socket.h> header file.  */
+#define HAVE_SYS_SOCKET_H 1
+
+/* Define if you have the <sys/time.h> header file.  */
+#define HAVE_SYS_TIME_H 1
+
+/* Define if you have the <sys/types.h> header file.  */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Define if you have the pthread library (-lpthread).  */
+#define HAVE_LIBPTHREAD 1
+
+/* Name of package */
+#define PACKAGE "prozilla"
+
+/* Version number of package */
+#define VERSION "1.3.6"
+
